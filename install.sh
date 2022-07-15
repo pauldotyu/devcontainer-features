bbaaaf63b1a0ff9673490f67945000181c7f3b48
Paul Yu <paul.d.yu@gmail.com>
2022-07-15 15:57:00 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,55 +11,19 @@ set -a
 set +a
 
 
-if [ ! -z ${_BUILD_ARG_HELLOWORLD} ]; then
-    echo "Activating feature 'helloworld'"
+if [ ! -z ${_BUILD_ARG_AZEXTENSION} ]; then
+    echo "Installing Azure CLI extension"
 
     # Build args are exposed to this entire feature set following the pattern:  _BUILD_ARG_<FEATURE ID>_<OPTION NAME>
-    GREETING=${_BUILD_ARG_HELLOWORLD_GREETING:-undefined}
+    NAME=${_BUILD_ARG_AZEXTENSION_NAME:-undefined}
 
-    tee /usr/hello.sh > /dev/null \
+    tee /usr/azextension.sh > /dev/null \
     << EOF
     #!/bin/bash
-    RED='\033[0;91m'
-    NC='\033[0m' # No Color
-    echo -e "\${RED}${GREETING}, \$(whoami)!"
-    echo -e "\${NC}"
+    az extension add --name ${NAME} -y
 EOF
 
-    chmod +x /usr/hello.sh
-    sudo cat '/usr/hello.sh' > /usr/local/bin/hello
-    sudo chmod +x /usr/local/bin/hello
-fi
-
-
-if [ ! -z ${_BUILD_ARG_COLOR} ]; then
-    echo "Activating feature 'color'"
-
-    # Build args are exposed to this entire feature set following the pattern:  _BUILD_ARG_<FEATURE ID>_<OPTION NAME>
-
-    if [ "${_BUILD_ARG_COLOR_FAVORITE}" == "red" ]; then
-        FAVORITE='\\033[0\;91m'
-    fi
-
-    if [ "${_BUILD_ARG_COLOR_FAVORITE}" == "green" ]; then
-        FAVORITE='\\033[0\;32m'
-    fi
-
-    if [ "${_BUILD_ARG_COLOR_FAVORITE}" == "gold" ]; then
-        FAVORITE='\\033[0\;33m'
-    fi
-
-    tee /usr/color.sh > /dev/null \
-    << EOF
-    #!/bin/bash
-    NC='\033[0m' # No Color
-
-    FAVORITE=${FAVORITE}
-    echo -e "\${FAVORITE} This is my favorite color! \${NC}"
-EOF
-
-    chmod +x /usr/color.sh
-    sudo cat '/usr/color.sh' > /usr/local/bin/color
-    sudo chmod +x /usr/local/bin/color
-
+    chmod +x /usr/azextension.sh
+    sudo cat '/usr/azextension.sh' > /usr/local/bin/azextension
+    sudo chmod +x /usr/local/bin/azextension
 fi
